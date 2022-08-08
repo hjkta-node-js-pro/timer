16f02d2512b9300e3f657e06432949962262cffd
hjkta <fux.aleksander@yandex.ru>
2022-08-09 00:05:59 +0300

    update app for showing noty instead of loggin

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import process from "process";
 import { parseTime } from "./parseTime.js";
+import notifier from "node-notifier";
 
 const [interval] = process.argv.slice(2);
 
@@ -8,5 +9,5 @@ const minutes = parseTime(interval, "m");
 const seconds = parseTime(interval, "s");
 
 setTimeout(() => {
-	console.log("It's time");
+	notifier.notify("It's time");
 }, (hours * 3600 + minutes * 60 + seconds) * 1000);
